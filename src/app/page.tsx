fe2249f6d12b26c988576e4ae22b0891441da9d3
André Ferreira <andre.ferreira.alves@gmail.com>
2026-01-28 12:58:23 -0300

    mais algumas páginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-// import Image from "next/image";
+import Image from "next/image";
+import heroImage from "@/assets/hero.svg";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Principal</h1>
+    <main className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)]">
+      <h2 className="font-medium text-2xl mb-2">Gerencie sua empresa</h2>
+      <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">
+        Atendimento, clientes
+      </h1>
+      <Image
+        src={heroImage}
+        alt="Image Hero"
+        className="max-w-sm md:max-w-xl"
+      />
     </main>
   );
 }
