bf4c79c26f4da23d01ec3079693f98eed6571b0c
André Ferreira <andre.ferreira.alves@gmail.com>
2026-02-06 13:14:18 -0300

    table de dados

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,42 @@
 import { Container } from "@/components/container";
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth/next";
-import { redirect } from "next/navigation";
+import Link from "next/link";
+import { TicketItem } from "./components/ticket";
 
 export default async function Dashboard() {
   // const session = await getServerSession(authOptions);
-
   // if (!session || !session.user) return redirect("/");
 
   return (
     <Container>
-      <h1>página dashboard</h1>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Chamados</h1>
+          <Link
+            href="/dashboard/new"
+            className="bg-blue-500 px-4 py-1 rounded text-white"
+          >
+            Abrir chamado
+          </Link>
+        </div>
+
+        <table className="min-w-full my-2">
+          <thead>
+            <tr>
+              <th className="font-medium text-left pl-2">CLIENTE</th>
+              <th className="font-medium text-left hidden sm:table-cell">
+                DATA CADASTRO
+              </th>
+              <th className="font-medium text-left">STATUS</th>
+              <th className="font-medium text-left">#</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <TicketItem />
+            <TicketItem />
+          </tbody>
+        </table>
+      </main>
     </Container>
   );
 }
