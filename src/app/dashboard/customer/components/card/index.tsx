8b99978ac3ba339a62ffd2518e5e58320c08543d
André Ferreira <andre.ferreira.alves@gmail.com>
2026-02-08 18:12:07 -0300

    customer

---
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -0,0 +1,22 @@
+export function CardCustomer() {
+  return (
+    <article className="flex flex-col bg-gray-100 border p-2 rounded-lg gap-2 hover:scale-105 duration-300">
+      <h2>
+        <a className="font-bold">Nome:</a>
+        Mercado
+      </h2>
+      <p>
+        <a className="font-bold">Email:</a>
+        texte@teste.com
+      </p>
+      <p>
+        <a className="font-bold">Telefone:</a>
+        (17) aaaa
+      </p>
+
+      <button className="bg-red-500 px-4 rounded text-white mt-2 self-start">
+        Deletar
+      </button>
+    </article>
+  );
+}
--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -1,12 +1,26 @@
 import { Container } from "@/components/container";
+import Link from "next/link";
+import { CardCustomer } from "./components/card";
 
 export default function Customer() {
   return (
     <Container>
-      <main>
-        <div>
-          <h1>Clientes</h1>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Clientes</h1>
+          <Link
+            href="/dashboard/customer/new"
+            className="bg-blue-500 text-white px-4 py-1 rounded"
+          >
+            Novo cliente
+          </Link>
         </div>
+
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-6">
+          <CardCustomer />
+          <CardCustomer />
+          <CardCustomer />
+        </section>
       </main>
     </Container>
   );
