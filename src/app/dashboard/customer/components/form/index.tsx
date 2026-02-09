fbb9fe41870737c9b4eb910cc65c6fae3d4e8212
André Ferreira <andre.ferreira.alves@gmail.com>
2026-02-09 13:32:52 -0300

    react hook forms, uso

---
+++ src/app/dashboard/customer/components/form/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/input";
+
+const schema = z.object({
+  name: z.string().min(1, "O campo é obrigátorio"),
+  email: z.email("Digite um e-mail válido").min(1, "O email é obrigatório"),
+  phone: z.string().refine((value) => {
+    return (
+      /^(?:\(\d{2}\)\s?)?\d{9}$/.test(value) ||
+      /^\d{2}\s\d{9}$/.test(value) ||
+      /^\d{11}$/.test(value)
+    );
+  }, "O número de telefone deve estar (DD) 999999999"),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function NewCustomerForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <form className="flex flex-col mt-6">
+      <label className="mb-1 text-lg font-medium">Nome completo</label>
+      <Input
+        type="text"
+        name="name"
+        placeholder="Digite o nome completo"
+        error={errors.name?.message}
+        register={register}
+      />
+    </form>
+  );
+}
--- src/app/dashboard/customer/new/page.tsx
+++ src/app/dashboard/customer/new/page.tsx
@@ -1,9 +1,23 @@
 import { Container } from "@/components/container";
+import Link from "next/link";
+import { NewCustomerForm } from "../components/form";
 
 export default function NewCustomer() {
   return (
     <Container>
-      <h1>new customer</h1>
+      <main className="flex flex-col mt-9 mb-2">
+        <div className="flex items-center gap-3">
+          <Link
+            href="/dashboard/customer"
+            className="bg-gray-900 px-4 py-2 text-white rounded"
+          >
+            Voltar
+          </Link>
+          <h1 className="text-3xl font-bold">Novo cliente</h1>
+        </div>
+
+        <NewCustomerForm />
+      </main>
     </Container>
   );
 }
