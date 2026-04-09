977caa897b518d82ab59ad8e389965cc31cb40e3
André Ferreira <andre.ferreira.alves@gmail.com>
2026-04-09 13:25:13 -0300

    tela de novos chamados

---
+++ src/app/dashboard/new/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/container";
+
+export default function NewTicket() {
+  return (
+    <Container>
+      <div className="flex items-center gatp-3"></div>
+    </Container>
+  );
+}
